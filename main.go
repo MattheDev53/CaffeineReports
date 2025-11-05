d4dea71b3d3ce65030afb2e89c752cd5362478be
MattheDev53 <matthew.c.albrecht@gmail.com>
2025-11-05 16:37:47 -0600

    You can now Generate Reports

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	typst "github.com/Dadido3/go-typst"
 )
 
 func NoImp() {
@@ -15,12 +16,46 @@ func PrintUsage() {
 	filename := argZeroList[len(argZeroList)-1]
 	fmt.Printf("Usage of %s:\n", filename)
 	fmt.Printf("%s help : Prints this help message\n", filename)
-	fmt.Printf("%s generate [data] [template] [output] : generates a report from the provided data and template\n", filename)
+	fmt.Printf("%s generate [template] [data] [output] : generates a report from the provided data and template\n", filename)
 }
 
-func GenerateReport(data string, template string, output string) {
-	NoImp()
-	fmt.Printf("Generating report from data '%s', template '%s' and output '%s'\n", data, template, output)
+func GenerateReport(templateFile string, dataFile string, outputFile string) {
+	fmt.Printf("Template: %s\n", templateFile)
+	fmt.Printf("Data:     %s\n", dataFile)
+	fmt.Printf("Output:   %s\n", outputFile)
+
+	inputs := map[string]string{
+		"data": dataFile,
+	}
+	options := typst.CLIOptions{
+		Input: inputs,
+	}
+
+	typstCLI := typst.CLI{}
+
+	in, err := os.Open(templateFile);
+
+	if err != nil {
+		fmt.Printf("Error reading %s: %v\n", templateFile, err)
+		os.Exit(1)
+	}
+	defer in.Close()
+
+	out, err := os.Create(outputFile)
+
+	if err != nil {
+		fmt.Printf("Error creating %s: %v\n", outputFile, err)
+		os.Exit(1)
+	}
+	defer out.Close()
+
+	fmt.Println("Compiling Report...")
+	if err := typstCLI.Compile(in, out, &options); err != nil {
+		fmt.Printf("Error compiling report: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Report Generated Successfully!")
+	fmt.Printf("file://%s\n", outputFile)
 }
 
 func main() {
